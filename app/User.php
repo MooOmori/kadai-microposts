3b20603e106439b7775a76e3189f0e4edca37133
EC2 Default User <ec2-user@ip-172-31-10-4.us-east-2.compute.internal>
2018-06-18 05:10:17 +0000

    timeline

--- app/User.php
+++ app/User.php
@@ -33,7 +33,7 @@ class User extends Authenticatable
     }
     
     
-        public function followings()
+    public function followings()
     {
         return $this->belongsToMany(User::class, 'user_follow', 'user_id', 'follow_id')->withTimestamps();
     }
@@ -42,6 +42,8 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(User::class, 'user_follow', 'follow_id', 'user_id')->withTimestamps();
     }
+    
+    
     
         public function follow($userId)
     {
@@ -81,6 +83,14 @@ class User extends Authenticatable
     
     public function is_following($userId) {
         return $this->followings()->where('follow_id', $userId)->exists();
+    }
+    
+    
+         public function feed_microposts()
+    {
+        $follow_user_ids = $this->followings()-> pluck('users.id')->toArray();
+        $follow_user_ids[] = $this->id;
+        return Micropost::whereIn('user_id', $follow_user_ids);
     }
     
 }
